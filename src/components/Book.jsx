fc1f38fdbcda598120feb3ff5f1865cbf5ac5068
yoojene <eugene.cross@gmail.com>
2021-02-16 21:42:01 +0000

    Adding spacing between list items

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Card from 'react-bootstrap/Card';
 import Author from './Author';
+import cx from 'classnames';
+import styles from '../styles/Book.module.css';
 const Book = ({ post }) => {
   return (
-    <Card>
+    <Card className={cx(styles.Book)}>
       <Card.Title>
         <Author author={post.author}></Author>
       </Card.Title>
